58c0c0dab1f8822caaedc1ba220d633ac2a49ac9
averydustin <swimmer2>
2021-03-15 10:49:47 -0600

    Initial check-in of module IS413Project1

---
+++ IS413Project1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS413Project1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
