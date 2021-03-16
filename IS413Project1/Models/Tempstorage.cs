44f11115bb3ab37e21f49f8cab9e3d3e16c705ea
syabutan <71088052+syabutan@users.noreply.github.com>
2021-03-16 14:07:23 -0600

    timeInput

---
+++ IS413Project1/Models/Tempstorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS413Project1.Models
+{
+    public class Tempstorage
+    {
+    }
+}
