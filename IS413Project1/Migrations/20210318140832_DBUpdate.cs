117706bb25d6054f8f0458420853e5c13956068d
parkergunnell <parkerlg@byu.edu>
2021-03-18 08:12:28 -0600

    Database please work

---
+++ IS413Project1/Migrations/20210318140832_DBUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IS413Project1.Migrations
+{
+    public partial class DBUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
