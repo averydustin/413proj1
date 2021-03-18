227f565538a1abda53ef06d5adbf536647da92a9
parkergunnell <parkerlg@byu.edu>
2021-03-17 20:22:17 -0600

    Fix View Model

---
+++ IS413Project1/Models/ViewModels/TimeSlotViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IS413Project1.Models.ViewModels
+{
+    public class TimeSlotViewModel
+    {
+        public IEnumerable<TimeSlot> TimeSlots { get; set; }
+        public IEnumerable<Appointment> Appointments { get; set; }
+        
+        
+    }
+}
